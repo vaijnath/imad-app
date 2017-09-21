b986621df9cff3270280d8d14a5a7de4dffd8762
mukund55 <31120420+mukund55@users.noreply.github.com>
2017-09-21 23:32:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('article one requested here and sered here');
+  res.send('article one requested here and served here');
 });
 app.get('/article-two', function (req, res) {
-  res.send('article two requested here and sered here' );
+  res.send('article two requested here and served here' );
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
