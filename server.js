2799e420685fe87bf7ff6b9f87ec10bad3d89efe
mukund55 <31120420+mukund55@users.noreply.github.com>
2017-09-21 23:23:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article', function (req, res) {
   res.send('i will have and sered aricle one here' );
